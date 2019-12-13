90e833825419e11113af2bd58be72659f1801b23
BoAn Chen <troy0606troy0606@gmail.com>
2019-12-13 13:02:20 +0800

    加註解

--- ProjectReact/src/components/cart/CartLeft.js
+++ ProjectReact/src/components/cart/CartLeft.js
@@ -6,6 +6,7 @@ import { useAlert } from "react-alert";
 import Cards from "react-credit-cards";
 import "react-credit-cards/lib/styles.scss";
 
+// 購物車信用卡介面
 export class PaymentForm extends React.Component {
   constructor(props) {
     super(props);
@@ -17,15 +18,19 @@ export class PaymentForm extends React.Component {
       number: ""
     };
   }
+  // 信用卡狀態
 
   handleInputFocus = e => {
     this.setState({ focus: e.target.name });
   };
+  // setState 欄位focus
 
   handleInputChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
+  // setState 解構事件目標
+  // setState (事件目標名稱 : 輸入的值)
 
   render() {
     return (
@@ -49,6 +54,7 @@ export class PaymentForm extends React.Component {
             maxLength={16}
             onChange={this.handleInputChange}
             onFocus={this.handleInputFocus}
+            // 如果沒有勾選信用卡(狀態)則無法輸入
             disabled={!this.props.creditRadio}
           />
           <input
@@ -90,8 +96,11 @@ const CartLeft = ({
 }) => {
   const alert = useAlert();
   const [cartTotal, setCartTotal] = useState(0);
+  // 購物車總價
   const [fnCartTotal, setFnCartTotal] = useState(0);
+  // 購物車最終總價(含優惠卷或紅利)
   const [coupon, setCoupon] = useState(0);
+  // 使用的優惠卷編號
   const [couponUse, setCouponUse] = useState(0);
   const [bonusUse, setBonusUse] = useState(0);
   const [bonus, setBonus] = useState(0);
@@ -212,17 +221,23 @@ const CartLeft = ({
 
   useEffect(() => {
     Promise.all([setPage(4), getBonus(), getCoupon(), getBonusStandard()]);
+    // 頁面載入時抓取紅利/優惠卷/紅利標準
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
+    // couponSelect > 10 ? couponSelect / 100 : couponSelect / 10 判斷折數為兩位獲一位，回傳
     const couponAfter =
       cartTotal * (couponSelect > 10 ? couponSelect / 100 : couponSelect / 10);
+      // 購物車*折扣數
     if (bonus) {
+      // 如果有使用紅利
+      // 設定最終購物車-紅利
       setFnCartTotal(Math.floor(couponAfter) - bonus);
     } else {
       setFnCartTotal(Math.floor(couponAfter));
     }
+    // 選取優惠卷或紅利後 componentDidUpdate
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [couponSelect, bonus]);
 
@@ -234,6 +249,7 @@ const CartLeft = ({
             <div
               className="d-flex align-items-center"
               onClick={() => setStep(0)}
+              // 點擊時跑到第一步
             >
               <div className="checkPageIcon cartStep">1</div>
               <h5 style={{ color: "#f78177", fontWeight: "bold" }}>
@@ -244,6 +260,7 @@ const CartLeft = ({
             <div
               className="d-flex align-items-center"
               onClick={() => {
+                // 如果課程或食材購物車不為空則點擊時跑到第二步，否則留第一步
                 courseCards.length || ingreCards.length
                   ? setStep(1)
                   : setStep(0);
@@ -273,6 +290,7 @@ const CartLeft = ({
                 <ul className="mt-4 w-100">
                   {step ? (
                     <>
+                    {/* 如果step為true則顯示以下內容 */}
                       <li>
                         <p>可用優惠卷</p>
                         {couponUse.length ? (
@@ -282,6 +300,7 @@ const CartLeft = ({
                               coponSelect(e);
                             }}
                           >
+                          {/* 如果有可使用優惠卷，則跑map迴圈顯示下拉選單 */}
                             {couponUse.map((coupon, index = 0) => {
                               return (
                                 <option index={index} value={coupon.coupon_sid}>
@@ -298,6 +317,7 @@ const CartLeft = ({
                         <li>
                           <p>可用折扣</p>
                           <h4>{couponSelect}折</h4>
+                          {/* 顯示使用折扣 */}
                         </li>
                       ) : (
                         ""
@@ -334,6 +354,7 @@ const CartLeft = ({
                   <h4>
                     ${" "}
                     {step ? (fnCartTotal ? fnCartTotal : cartTotal) : cartTotal}
+                    {/* 第二步的話，顯示(如最終購物車有值則顯示最終購物車，否則顯示購物車總價) */}
                   </h4>
                 </div>
                 <p style={{ color: "white", fontWeight: "bold" }}>
@@ -343,6 +364,7 @@ const CartLeft = ({
                       ? Math.ceil(fnCartTotal * bonusStandard)
                       : Math.ceil(cartTotal * bonusStandard)
                     : Math.ceil(cartTotal * bonusStandard)}
+                    {/* 可獲得紅利為 (如最終購物車有值則顯示最終購物車*紅利標準，否則顯示購物車總價*紅利標準)*/}
                 </p>
                 <p style={{ color: "white" }}>紅利計算率: {bonusStandard}</p>
                 <p style={{ color: "white" }}>紅利截止日期: {bonusDuration}</p>
@@ -359,11 +381,13 @@ const CartLeft = ({
                   name="pay"
                   onChange={() => {
                     setCreditRadio(!creditRadio);
+                    // 設定解鎖信用卡輸入
                   }}
                 />
                 <p>使用信用卡</p>
               </div>
               <PaymentForm creditRadio={creditRadio} />
+              {/* 上方信用卡在這顯示 */}
             </div>
           </>
         ) : (
@@ -373,6 +397,7 @@ const CartLeft = ({
           <button
             onClick={() => {
               courseCards.length || ingreCards.length ? setStep(1) : setStep(0);
+              // 如果購物車有東西才可去下一步
             }}
           >
             NEXT
